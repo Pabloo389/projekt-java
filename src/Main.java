6923cc79d8a1982932749189dd3a6b7d6bb8afba
Paweł <paweberg700@gmail.com>
2025-06-24 18:51:29 +0200

    Dodano nagłówek B do Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+	System.out.println("=== HEADER B ===");
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
