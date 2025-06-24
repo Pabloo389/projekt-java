bae95d5c30f85fc33086d845dd5bbb4ddd363bbb
Paweł <paweberg700@gmail.com>
2025-06-24 19:24:15 +0200

    BREAKING CHANGE: zmieniono menu - usunięto opcję Pomoc

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,8 @@ public class Main {
         while (running) {
             System.out.println("\n=== MENU ===");
             System.out.println("1. Start");
-            System.out.println("2. Pomoc");
-            System.out.println("3. O programie");
-            System.out.println("4. Wyjście");
+            System.out.println("2. O programie");
+            System.out.println("3. Wyjście");
             System.out.print("Wybierz opcję: ");
 
             int choice = scanner.nextInt();
@@ -20,13 +19,10 @@ public class Main {
                 case 1:
                     System.out.println("Uruchamianie aplikacji...");
                     break;
-                case 2:
-                    System.out.println("Pomoc techniczna zostanie wkrótce dodana.");
-                    break;
-		case 3:
+		case 2:
     		    System.out.println("To jest przykładowa aplikacja w Javie.");
     		    break;
-                case 4:
+                case 3:
                     System.out.println("Do widzenia!");
                     running = false;
                     break;
